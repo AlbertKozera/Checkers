b2ba092db1599f6dcd05142a035ae1c209261c33
Albert Kozera <albertkozera97@gmail.com>
2020-04-24 06:41:48 +0200

    - dodano obrazki dla figur gry - zmieniono nazwe glownej sceny

--- Warcaby/Warcaby/MainStage.Designer.cs
+++ Warcaby/Warcaby/MainStage.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Warcaby
 {
-    partial class Form1
+    partial class MainStage
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainStage));
             this.ucOptions = new Warcaby.Forms.UCOptions();
             this.ucNewGame = new Warcaby.Forms.UCNewGame();
             this.ucMainMenu = new Warcaby.Forms.UCMainMenu();
