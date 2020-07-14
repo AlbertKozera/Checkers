eae0fff68f39f90fc5915692916851fc5159e16d
pioste <pioste97@gmail.com>
2020-07-14 02:26:37 +0200

    Dodano klase OptionService, do ktorej zostaly przeniesione metody z UCOptions odpowiedzialne za zapis do pliku i odczyt z pliku oraz dzialanie checkBox'ow

--- Warcaby/Warcaby/CSharp/Forms/UCOptions.cs
+++ Warcaby/Warcaby/CSharp/Forms/UCOptions.cs
@@ -9,27 +9,20 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Windows.Forms;
 using System.Reflection;
+using Warcaby.CSharp.Service;
 
 namespace Warcaby.Forms
 {
     public partial class UCOptions : UserControl
     {
+        OptionService optionService = new OptionService();
         string filePath = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory);
-        
-                
+
         public UCOptions()
         {
             InitializeComponent();
-            filePath = Directory.GetParent(Directory.GetParent(filePath).FullName).FullName;
-            filePath += @"\Resources\OptionsState.txt";
+            filePath = optionService.CreateFileAndGetPath(filePath).ToString();
         }
-        private void EditLine(string newText, string fileName, int line) //Funkcja odpowiednia za edycje wskazanej linii
-        {
-            string[] readLine = File.ReadAllLines(fileName);
-            readLine[line - 1] = newText;
-            File.WriteAllLines(fileName, readLine);
-        }
-
         private void backToMenu(object sender, EventArgs e)
         {
             Controls.Clear();
@@ -37,48 +30,17 @@ namespace Warcaby.Forms
             Controls.Add(ucMainMenu);
             ucMainMenu.Show();
         }
-
         private void checkBoxThreadYes_CheckedChanged(object sender, EventArgs e)
         {
-            if (checkBoxThread.Checked)
-            {
-                EditLine("Checked", filePath, 1);
-            } else 
-            {
-                EditLine("Unchecked", filePath, 1);
-            }
+            optionService.CheckBoxMultiThreading(checkBoxThread ,filePath);
         }
-
         private void UCOptions_Load(object sender, EventArgs e)
         {
-            string readFirstLine = File.ReadLines(filePath).First();
-            if (readFirstLine == "Checked")
-            {
-                checkBoxThread.Checked = true;
-            } 
-            else
-            {
-                checkBoxThread.Checked = false;
-            }
-            string readSecondLine = File.ReadLines(filePath).Skip(1).First();
-            if (readSecondLine == "Checked")
-            {
-                checkBoxStartingGame.Checked = true;
-            } else
-            {
-                checkBoxStartingGame.Checked = false;
-            }
+            optionService.LoadCheckBoxes(checkBoxThread, checkBoxStartingGame, filePath);
         }
-
         private void CheckBoxStartingGame_CheckedChanged(object sender, EventArgs e)
         {
-            if (checkBoxStartingGame.Checked)
-            {
-                EditLine("Checked", filePath, 2);
-            } else
-            {
-                EditLine("Unchecked", filePath, 2);
-            }
+            optionService.ChangeBoxStartingGame(checkBoxStartingGame, filePath);
         }
     }
 }
